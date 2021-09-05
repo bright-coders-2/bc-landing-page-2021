0a2e333e32a02e69f592f24a9f15eadd7b55a75d
SampabloC <sampablo-jisc@outlook.com>
2021-09-04 19:24:08 -0500

    Fixed Navbar Change font type Fix effect when mouse is over Fix tablet trouble when screen rotate Fix responsive logo
    Co-authored-by: AlejandroVI99 <avi200599@gmail.com>

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -4,14 +4,27 @@ document.addEventListener("turbolinks:load", function () {
   let imageIndex = 0;
   let logoId = document.getElementById("logo");
   let className = document.getElementById("button");
+  let logoContainer = document.getElementById("logo-container");
   className.onclick = function () {
     if (imageIndex == 0) {
       logoId.className = 'logo-open';
+      logoContainer.className = 'logo-container-open';
     } else {
       logoId.className = 'logo-closed';
+      logoContainer.className = 'logo-container-close';
     }
     imageIndex = (imageIndex + images.length + 1) % (images.length);
     document.getElementById("logo").setAttribute('src', logos[imageIndex]);
     document.getElementById("image").setAttribute('src', images[imageIndex]);
   }
+
+  window.addEventListener("resize", function(){
+    if (window.matchMedia("(min-width: 992px)").matches){
+      document.getElementById("logo").setAttribute('src', logos[0]);
+    }else{
+      if (!document.getElementById('button').classList.contains('collapsed')) {
+        document.getElementById("logo").setAttribute('src', logos[1]);
+      }
+    }
+  });
 });
